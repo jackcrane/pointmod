b35f0736982da89067b1e472539c3c853ef3ad2f
Jack Crane <jack@jackcrane.rocks>
2026-03-25 18:58:19 -0500

    idk at this point

--- src/OpenGL.cpp
+++ src/OpenGL.cpp
@@ -1,35 +1,12 @@
 #include "OpenGL.hpp"
 
-#if !defined(__APPLE__)
-#define GLFW_INCLUDE_NONE
-#include <GLFW/glfw3.h>
-
-namespace pointmod::gl {
-
-#define POINTMOD_DEFINE_GL_FUNCTION(type, name) type name = nullptr;
-POINTMOD_OPENGL_FUNCTIONS(POINTMOD_DEFINE_GL_FUNCTION)
-#undef POINTMOD_DEFINE_GL_FUNCTION
-
-}  // namespace pointmod::gl
-#endif
-
 namespace pointmod {
 
 bool InitializeOpenGLBindings() {
 #if defined(__APPLE__)
   return true;
 #else
-  bool loaded = true;
-
-#define POINTMOD_LOAD_GL_FUNCTION(type, name) \
-  gl::name = reinterpret_cast<type>(glfwGetProcAddress(#name)); \
-  loaded = loaded && gl::name != nullptr;
-
-  POINTMOD_OPENGL_FUNCTIONS(POINTMOD_LOAD_GL_FUNCTION)
-
-#undef POINTMOD_LOAD_GL_FUNCTION
-
-  return loaded;
+  return imgl3wInit() == 0;
 #endif
 }
 
--- src/OpenGL.hpp
+++ src/OpenGL.hpp
@@ -3,17 +3,7 @@
 #if defined(__APPLE__)
 #include <OpenGL/gl3.h>
 #else
-#if defined(_WIN32)
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
-#include <windows.h>
-#endif
-#include <GL/gl.h>
-#include <GL/glext.h>
+#include <backends/imgui_impl_opengl3_loader.h>
 #endif
 
 namespace pointmod {
@@ -21,68 +11,3 @@ namespace pointmod {
 bool InitializeOpenGLBindings();
 
 }  // namespace pointmod
-
-#if !defined(__APPLE__)
-#define POINTMOD_OPENGL_FUNCTIONS(X) \
-  X(PFNGLATTACHSHADERPROC, glAttachShader) \
-  X(PFNGLBINDATTRIBLOCATIONPROC, glBindAttribLocation) \
-  X(PFNGLBINDBUFFERPROC, glBindBuffer) \
-  X(PFNGLBINDVERTEXARRAYPROC, glBindVertexArray) \
-  X(PFNGLBUFFERDATAPROC, glBufferData) \
-  X(PFNGLCOMPILESHADERPROC, glCompileShader) \
-  X(PFNGLCREATEPROGRAMPROC, glCreateProgram) \
-  X(PFNGLCREATESHADERPROC, glCreateShader) \
-  X(PFNGLDELETEBUFFERSPROC, glDeleteBuffers) \
-  X(PFNGLDELETEPROGRAMPROC, glDeleteProgram) \
-  X(PFNGLDELETESHADERPROC, glDeleteShader) \
-  X(PFNGLDELETEVERTEXARRAYSPROC, glDeleteVertexArrays) \
-  X(PFNGLENABLEVERTEXATTRIBARRAYPROC, glEnableVertexAttribArray) \
-  X(PFNGLGENBUFFERSPROC, glGenBuffers) \
-  X(PFNGLGENVERTEXARRAYSPROC, glGenVertexArrays) \
-  X(PFNGLGETPROGRAMINFOLOGPROC, glGetProgramInfoLog) \
-  X(PFNGLGETPROGRAMIVPROC, glGetProgramiv) \
-  X(PFNGLGETSHADERINFOLOGPROC, glGetShaderInfoLog) \
-  X(PFNGLGETSHADERIVPROC, glGetShaderiv) \
-  X(PFNGLGETUNIFORMLOCATIONPROC, glGetUniformLocation) \
-  X(PFNGLLINKPROGRAMPROC, glLinkProgram) \
-  X(PFNGLSHADERSOURCEPROC, glShaderSource) \
-  X(PFNGLUNIFORM1FPROC, glUniform1f) \
-  X(PFNGLUNIFORMMATRIX4FVPROC, glUniformMatrix4fv) \
-  X(PFNGLUSEPROGRAMPROC, glUseProgram) \
-  X(PFNGLVERTEXATTRIBPOINTERPROC, glVertexAttribPointer)
-
-namespace pointmod::gl {
-
-#define POINTMOD_DECLARE_GL_FUNCTION(type, name) extern type name;
-POINTMOD_OPENGL_FUNCTIONS(POINTMOD_DECLARE_GL_FUNCTION)
-#undef POINTMOD_DECLARE_GL_FUNCTION
-
-}  // namespace pointmod::gl
-
-#define glAttachShader pointmod::gl::glAttachShader
-#define glBindAttribLocation pointmod::gl::glBindAttribLocation
-#define glBindBuffer pointmod::gl::glBindBuffer
-#define glBindVertexArray pointmod::gl::glBindVertexArray
-#define glBufferData pointmod::gl::glBufferData
-#define glCompileShader pointmod::gl::glCompileShader
-#define glCreateProgram pointmod::gl::glCreateProgram
-#define glCreateShader pointmod::gl::glCreateShader
-#define glDeleteBuffers pointmod::gl::glDeleteBuffers
-#define glDeleteProgram pointmod::gl::glDeleteProgram
-#define glDeleteShader pointmod::gl::glDeleteShader
-#define glDeleteVertexArrays pointmod::gl::glDeleteVertexArrays
-#define glEnableVertexAttribArray pointmod::gl::glEnableVertexAttribArray
-#define glGenBuffers pointmod::gl::glGenBuffers
-#define glGenVertexArrays pointmod::gl::glGenVertexArrays
-#define glGetProgramInfoLog pointmod::gl::glGetProgramInfoLog
-#define glGetProgramiv pointmod::gl::glGetProgramiv
-#define glGetShaderInfoLog pointmod::gl::glGetShaderInfoLog
-#define glGetShaderiv pointmod::gl::glGetShaderiv
-#define glGetUniformLocation pointmod::gl::glGetUniformLocation
-#define glLinkProgram pointmod::gl::glLinkProgram
-#define glShaderSource pointmod::gl::glShaderSource
-#define glUniform1f pointmod::gl::glUniform1f
-#define glUniformMatrix4fv pointmod::gl::glUniformMatrix4fv
-#define glUseProgram pointmod::gl::glUseProgram
-#define glVertexAttribPointer pointmod::gl::glVertexAttribPointer
-#endif
