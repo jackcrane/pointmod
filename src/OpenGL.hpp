93b63756d0cb6733147f1ba18eaa987a7e907d73
Jack Crane <jack@jackcrane.rocks>
2026-03-25 18:53:46 -0500

    Fix another issue building?

--- src/OpenGL.hpp
+++ src/OpenGL.hpp
@@ -3,6 +3,15 @@
 #if defined(__APPLE__)
 #include <OpenGL/gl3.h>
 #else
+#if defined(_WIN32)
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#include <windows.h>
+#endif
 #include <GL/gl.h>
 #include <GL/glext.h>
 #endif
