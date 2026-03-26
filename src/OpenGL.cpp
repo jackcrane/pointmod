508703cd3e722e7ee878c88df1f9e559bb315cdf
Jack Crane <jack@jackcrane.rocks>
2026-03-25 19:01:57 -0500

    Fix gl and make release happen when i push to main

--- src/OpenGL.cpp
+++ src/OpenGL.cpp
@@ -1,12 +1,31 @@
 #include "OpenGL.hpp"
 
+#if !defined(__APPLE__)
+namespace pointmod::gl {
+
+PFNPOINTMODGLBINDATTRIBLOCATIONPROC BindAttribLocation = nullptr;
+PFNPOINTMODGLUNIFORM1FPROC Uniform1f = nullptr;
+PFNPOINTMODGLDRAWARRAYSPROC DrawArrays = nullptr;
+
+}  // namespace pointmod::gl
+#endif
+
 namespace pointmod {
 
 bool InitializeOpenGLBindings() {
 #if defined(__APPLE__)
   return true;
 #else
-  return imgl3wInit() == 0;
+  if (imgl3wInit() != 0) {
+    return false;
+  }
+
+  gl::BindAttribLocation = reinterpret_cast<PFNPOINTMODGLBINDATTRIBLOCATIONPROC>(imgl3wGetProcAddress("glBindAttribLocation"));
+  gl::Uniform1f = reinterpret_cast<PFNPOINTMODGLUNIFORM1FPROC>(imgl3wGetProcAddress("glUniform1f"));
+  gl::DrawArrays = reinterpret_cast<PFNPOINTMODGLDRAWARRAYSPROC>(imgl3wGetProcAddress("glDrawArrays"));
+  return gl::BindAttribLocation != nullptr &&
+    gl::Uniform1f != nullptr &&
+    gl::DrawArrays != nullptr;
 #endif
 }
 
--- src/OpenGL.hpp
+++ src/OpenGL.hpp
@@ -4,6 +4,38 @@
 #include <OpenGL/gl3.h>
 #else
 #include <backends/imgui_impl_opengl3_loader.h>
+
+#ifndef GL_DEPTH_BUFFER_BIT
+#define GL_DEPTH_BUFFER_BIT 0x00000100
+#endif
+
+#ifndef GL_POINTS
+#define GL_POINTS 0x0000
+#endif
+
+#ifndef GL_STATIC_DRAW
+#define GL_STATIC_DRAW 0x88E4
+#endif
+
+#ifndef GL_PROGRAM_POINT_SIZE
+#define GL_PROGRAM_POINT_SIZE 0x8642
+#endif
+
+typedef void(APIENTRYP PFNPOINTMODGLBINDATTRIBLOCATIONPROC)(GLuint program, GLuint index, const GLchar* name);
+typedef void(APIENTRYP PFNPOINTMODGLUNIFORM1FPROC)(GLint location, GLfloat v0);
+typedef void(APIENTRYP PFNPOINTMODGLDRAWARRAYSPROC)(GLenum mode, GLint first, GLsizei count);
+
+namespace pointmod::gl {
+
+extern PFNPOINTMODGLBINDATTRIBLOCATIONPROC BindAttribLocation;
+extern PFNPOINTMODGLUNIFORM1FPROC Uniform1f;
+extern PFNPOINTMODGLDRAWARRAYSPROC DrawArrays;
+
+}  // namespace pointmod::gl
+
+#define glBindAttribLocation pointmod::gl::BindAttribLocation
+#define glUniform1f pointmod::gl::Uniform1f
+#define glDrawArrays pointmod::gl::DrawArrays
 #endif
 
 namespace pointmod {
